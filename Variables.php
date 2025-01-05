16800563b786b8421cc473bcf8afefc01a95edc2
Gabriel Hernandez <gabrielemanuel.hdz@gmail.com>
2025-01-05 14:46:11 -0600

    Create Variables.php
    Variables en PHP (Integer, Float, Boolean, String)

---
+++ Variables.php
@@ -0,0 +1,16 @@
+<?php
+$nombre = "Gabriel"; //Cadena en php
+$edad = 20; // Entero en php
+$estatura =1.72; // Flotante en php
+$soltero = true; // Booleano en php
+
+//echo es para imprimir o mostrar en consola o pantalla en php
+
+echo $nombre;
+echo "\n"; //Salto de linea
+echo $edad;
+echo "\n"; //Los saltos de inea pueden ser con "\n" == "<br>"
+echo $estatura;
+echo "\n"; // En este caso use "\n" porque es para consola y "<br>" para servidor
+echo $soltero;
+?>
