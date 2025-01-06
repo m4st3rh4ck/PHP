6417425a34153dcd6064f3b99fdfd4056f72dc4b
Gabriel Hernandez <gabrielemanuel.hdz@gmail.com>
2025-01-05 20:03:26 -0600

    Create Operadores de Comparacion.php
    Operadores de comparacion en php

---
+++ Operadores de Comparacion.php
@@ -0,0 +1,19 @@
+<?php
+$numero1 = 20;
+$numero2 = 14;
+//mayor que >
+echo $numero1 > $numero2;
+echo "\n";
+//menor que <
+echo $numero2 < $numero1;
+echo "\n";
+//mayor igual que >=
+echo $numero1 >= $numero2;
+echo "\n";
+//menor igul que <=
+echo $numero2 <= $numero1;
+echo "\n";
+//distinto de !=
+echo $numero1 != $numero2;
+echo "\n";
+?>
