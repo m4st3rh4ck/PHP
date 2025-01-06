7f77c44c589e80154e60821bf60673f0bfe154cd
Gabriel Hernandez <gabrielemanuel.hdz@gmail.com>
2025-01-05 18:51:40 -0600

    Create Operadores Aritmetico.php
    Operadores Aritmeticos en php
    
    + , - , * , /, %

---
+++ Operadores Aritmetico.php
@@ -0,0 +1,20 @@
+<?php
+//Operadores Aritmeticos en PHP 
+$numero1 = 10;
+$numero2 = 5;
+
+//Suma
+echo $numero1 + $numero2;
+echo "\n";
+//Resta
+echo $numero1 - $numero2;
+echo "\n";
+//Multiplicacion
+echo $numero1 * $numero2;
+echo "\n";
+//Division
+echo $numero1 / $numero2;
+echo "\n";
+//Modulo
+echo $numero1 % $numero2;
+?>
