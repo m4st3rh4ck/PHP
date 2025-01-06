0bad561b7a4961fb60de2255145ed3d3a3f80a9b
Gabriel Hernandez <gabrielemanuel.hdz@gmail.com>
2025-01-05 19:03:43 -0600

    Create Operaores Logicos.php

---
+++ Operaores Logicos.php
@@ -0,0 +1,21 @@
+<?php
+//Operadores logicos en PHP
+$valor1 = true;
+$valor2 = false;
+$valor3 = true;
+//And
+echo $valor1 and $valor2; 
+echo "\n"; 
+echo $valor1 && $valor3;
+echo "\n";
+//Or
+echo $valor1 or $valor2;
+echo "\n";
+echo $valor1 || $valor3;
+//Not
+echo !$valor1;
+echo "\n";
+echo !$valor2;
+echo "\n";
+echo !$valor3;
+?>
